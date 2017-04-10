e87a9466c674206e01d624019b10cf063ce09300
kimnorth <benignhumour@gmail.com>
2017-04-10 19:08:49 +0100

    Refactored my code so I can add multiple cats easily. Took a while to get the order right that elements needed to be appended as children.

--- public/app.js
+++ public/app.js
@@ -1,31 +1,46 @@
-var app = function(){
+var addCat = function(inputName, inputFood, inputImage){
+  var cat = createUl();
+  var name = createLi(inputName);
+  var food = createLi(inputFood);
+  var image = createImage(inputImage);
+  appendElements(cat, name, food, image);
+}
 
-  // var cat = document.createElement('ul');
-  // cat.classList.add('cat');
-  // cat.appendChild(name);
-  // cat.appendChild(favouriteFood);
-
-  var name = document.createElement('li');
-  name.innerText = 'Name: Mittens';
-
-  var favouriteFood = document.createElement('li');
-  favouriteFood.innerText = 'Favourite food: Fish Fingers';
-
-  var pictureLi = document.createElement('li');
-  var picture = document.createElement('img');
-  picture.src = "http://www.childsplayaz.org/sysimg/large-Cat-Fish-JugglingonBall-WEB.jpg"
-  picture.width = "500"
-  pictureLi.appendChild(picture);
-
+var createUl = function(){
   var cat = document.createElement('ul');
   cat.classList.add('cat');
-  // cat.innerText = 'This is the ul'
-  cat.appendChild(name);
-  cat.appendChild(favouriteFood);
-  cat.appendChild(pictureLi);
+  return cat;
+}
 
+var createLi = function(text){
+  var newLi = document.createElement('li');
+  newLi.innerText = text;
+  return newLi;
+}
+
+var createImage = function(image){
+  var newLi = document.createElement('li');
+  var picture = document.createElement('img');
+  picture.src = image;
+  picture.width = "500";
+  newLi.appendChild(picture);
+  return newLi;
+}
+
+var appendElements = function(cat, name, food, image){
+  cat.appendChild(name);
+  cat.appendChild(food);
+  cat.appendChild(image);
   var cats = document.querySelector('#cats');
   cats.appendChild(cat);
+}
+
+var app = function(){
+
+  addCat("Name: Steve", "Favourite Food: Chips", "http://www.childsplayaz.org/sysimg/large-Cat-Fish-JugglingonBall-WEB.jpg");
+
+  addCat("Name: Serious Cat", "Favourite Food: Crisps", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/CatLolCatExample.jpg/220px-CatLolCatExample.jpg");
+  
 
 }
 
