ea3e5671de967fb657a6eca3d1af0a7c0338b728
kimnorth <benignhumour@gmail.com>
2017-04-10 19:14:21 +0100

    Removed all HTML cats and replaced them with JS-created cats.

--- public/app.js
+++ public/app.js
@@ -37,10 +37,16 @@ var appendElements = function(cat, name, food, image){
 
 var app = function(){
 
+  addCat("Name: Boba", "Favourite food: Sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg")
+
+  addCat("Name: Barnaby", "Favourite food: Tuna", "http://digitalspyuk.cdnds.net/15/44/640x426/gallery-cat-3.jpg")
+
+  addCat("Name: Max", "Favourite food: Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg")
+
   addCat("Name: Steve", "Favourite Food: Chips", "http://www.childsplayaz.org/sysimg/large-Cat-Fish-JugglingonBall-WEB.jpg");
 
   addCat("Name: Serious Cat", "Favourite Food: Crisps", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/CatLolCatExample.jpg/220px-CatLolCatExample.jpg");
-  
+
 
 }
 
